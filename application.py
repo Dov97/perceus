45336c730aa6a7f6920feb2953037fff9cb6f3be
David Sellars <dovsellars@gmail.com>
2023-05-30 00:49:49 +0100

    Add all required roots plus getUsers (all)

--- application.py
+++ application.py
@@ -83,6 +83,133 @@ else:
             print("got the following exception when attempting db.create_all(): " + str(exception))
 
 
+""" Routes """
+@app.route('/')
+def index():
+
+    return 'Hello Perceus!'
+
+
+@app.route('/users', methods=["POST"])
+def addUser():
+
+    user = User(
+        lastName=request.json['lastName'],
+        firstName=request.json['firstName'],
+        emails=[Email(mail=email) for email in request.json.get('emails', [])],
+        phoneNumbers=[PhoneNumber(number=number) for number in request.json.get('phoneNumbers', [])]
+    )
+
+    db.session.add(user)
+    db.session.commit()
+
+    return {'id': user.id}, 201
+
+
+@app.route('/users/')
+def getUsers():
+
+    users = User.query.all()
+
+    userDict = []
+    for user in users:
+        userDict.append({"lastName": user.lastName,
+                         "firstName": user.firstName,
+                         "emails": [email.mail for email in user.emails],
+                         'phoneNumbers': [phone.number for phone in user.phoneNumbers]})
+
+    return {"users" : userDict}
+
+
+@app.route('/users/<int:id>')
+def getUserById(id):
+
+    user = db.session.get(User, id)
+    if user is None:
+        abort(404)
+
+    userInfo = {"lastName": user.lastName,
+                "firstName": user.firstName,
+                "emails": [email.mail for email in user.emails],
+                'phoneNumbers': [phone.number for phone in user.phoneNumbers]}
+
+    return {"user" : userInfo}
+
+
+@app.route('/users/<string:firstName>/<string:lastName>')
+def getUserByName(firstName, lastName):
+
+    userFilterByLast = User.query.filter_by(lastName=lastName).first()
+    user = userFilterByLast.query.filter_by(firstName=firstName).first()
+
+    userInfo = {"lastName": user.lastName,
+                "firstName": user.firstName,
+                "emails": [email.mail for email in user.emails],
+                'phoneNumbers': [phone.number for phone in user.phoneNumbers]}
+
+    return {"user" : userInfo}
+
+
+@app.route('/users/<int:id>', methods=['DELETE'])
+def deleteUser(id):
+    user = db.session.get(User, id)
+    if user is None:
+        abort(404)
+
+    db.session.delete(user)
+    db.session.commit()
+
+    return {'message': f'Deleted user with id: {id}'}, 200
+
+
+@app.route('/users/<int:id>/add_email', methods=['POST'])
+def addUserEmail(id):
+
+    user = db.session.get(User, id)
+    if user is None:
+        abort(404)
+
+    new_email = [Email(mail=email) for email in request.json.get('emails', [])]
+
+    user.emails.extend(new_email)
+    db.session.commit()
+
+    return {'Email added': [email.mail for email in new_email]}, 201
+
+
+@app.route('/users/<int:id>/add_phone_number', methods=['POST'])
+def addPhoneNumber(id):
+
+    user = db.session.get(User, id)
+    if user is None:
+        abort(404)
+
+    new_number = [PhoneNumber(number=phone_number) for phone_number in request.json.get('phoneNumbers', [])]
+
+    user.phoneNumbers.extend(new_number)
+    db.session.commit()
+
+    return {'Number added': [phone.number for phone in new_number]}, 201
+
+
+@app.route('/users/<int:id>/update_email/<int:email_id>', methods=['POST'])
+def updateEmail(id, email_id):
+
+    user = User.query.get(id)
+    if user is None:
+        abort(404)
+
+    # Create new email.
+    new_email = request.json.get('emails')
+
+    # Assume that a user can have only one email.
+    if email_id < len(user.emails):
+        user.emails[email_id].mail = new_email
+    else:
+        user.emails.append(Email(mail=new_email))
+        db.session.commit()
+
+    return {'Email updated' : new_email}
 
 
 if __name__ == '__main__':
